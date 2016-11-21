987687b398759f9da1082244cf6d817d3799a52f
Philip Lijnzaad <plijnzaad@gmail.com>
2016-11-21 18:29:29 +0100

    started work on optimizing the barcodes

--- check-barcodes.pl
+++ check-barcodes.pl
@@ -9,13 +9,13 @@ use File::Basename;
 use mismatch;
 use Algorithm::Combinatorics;
 
-use vars qw($opt_h $opt_m);
+use vars qw($opt_h $opt_m $opt_o);
 
 $opt_m=1;
 
 my $Usage="Usage:
 
-   $0  [ -m NMISMATCHES ]  barcodes.txt
+   $0  [ -m NMISMATCHES ] [-o uniqified-barcodes.txt ]   barcodes.txt
 
 Given a barcodefile (format: id \\t barcode \\n), find all the barcodes
 that have ambiguous potential barcode misreads.  The output is 
@@ -28,7 +28,7 @@ MISREAD 'highlighted' in lowercase.
 written by <plijnzaad\@gmail.com>
 ";
 
-if ( !getopts("m:h") || $opt_h) {
+if ( !getopts("m:o:h") || $opt_h) {
     die $Usage; 
 }
 my  $allowed_mismatches = $opt_m;
@@ -51,13 +51,20 @@ allowing for $allowed_mismatches mismatches\n";
 my $iter= Algorithm::Combinatorics::tuples_with_repetition([ qw(A C G T) ] , $len);
 
 my $nunknown=0;
+my $nexact=0;
 my $nunique=0;
 my $nambiguous=0;
 my $badcodes={};
 
+WORD:
 while(my $inst=$iter->next()) {
-  my $w=join("",@$inst);
+  my $w=join("",@$inst);                # potential mismatched
   
+  if (exists( $barcodes->{$w})) { 
+    $nexact++;
+    next WORD;
+  }
+
   my @codes=mismatch::safe_rescue($w, $mismatch_REs);
 
   $nunknown +=  (@codes==0);
@@ -67,16 +74,26 @@ while(my $inst=$iter->next()) {
   if (@codes>1) {               # ambiguous
     my @mm = map { mismatch::format_mm($w, $_); } @codes;
     print "$w: ". join(' ', @mm) . "\n";
+    
+    for my $code (@codes) { 
+      my $mm=mismatch::format_mm($w, $code);
+      $badcodes->{$code}{$mm}++;
+    }
   }
+}                                       # WORD
 
-  for my $b (@codes) { 
-    push @{$badcodes->{$w}}, @codes;
-  }
+if ($opt_o) { 
+  open(FILE, "> $opt_o") || die "$opt_o: $!";
+
+  die "not yet ready";
+  close(FILE);
 }
 
+
 warn "\nstats:\n";
+warn "no mismatch: $nexact\n";
+warn "mismatched but unique: $nunique\n";
+warn "mismatched ambiguous: $nambiguous\n";
 warn "unknown: $nunknown\n";
-warn "unique: $nunique\n";
-warn "ambiguous: $nambiguous\n";
 warn "done\n";
 1;
